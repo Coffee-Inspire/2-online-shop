b54bc474ffabc7e76e212e6caa11467cdd4224b4
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-06-30 20:13:53 +0700

    clean code

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,16 +6,17 @@ import BarBrown from '../components/atoms/BarBrown'
 
 function DashboardPage() {
     return (
-        <Container fluid>
-            <Row>
-
+        <Container fluid className="d-flex flex-row ">
+            <Row className="w-25">
+                ini nav
             </Row>
-            <Row>
+            
+            <div className="w-100">
                 <BarBrown />
                 <Switch>
 
                 </Switch>
-            </Row>
+            </div>
         </Container>
     )
 }
