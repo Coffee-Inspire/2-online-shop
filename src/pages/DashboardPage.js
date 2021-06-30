b73a1bdc534ea7498b0a93d581d7cb7e798fe65f
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-06-30 20:07:39 +0700

    login page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
+
+import BarBrown from '../components/atoms/BarBrown'
 
 function DashboardPage() {
     return (
-        <div>
-            
-        </div>
+        <Container fluid>
+            <Row>
+
+            </Row>
+            <Row>
+                <BarBrown />
+                <Switch>
+
+                </Switch>
+            </Row>
+        </Container>
     )
 }
 
