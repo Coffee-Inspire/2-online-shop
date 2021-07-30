6da60d9eb1018a9babfd8ac28e638ed275419d03
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-07-30 23:15:54 +0700

    image priview + check file

--- src/components/molecules/FormHorizontalImage.js
+++ src/components/molecules/FormHorizontalImage.js
@@ -1,72 +1,98 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Row, Col, Form, Image, Button } from 'react-bootstrap';
 
 import imageNotFound from '../../assets/images/imgNotFound.jpg'
 
 function FormHorizontalImage(props) {
-    const uploadBtn = useRef(null)
+    const uploadBtn = useRef(null);
+    const uploadDrag = useRef(null);
 
-    const checkFile = (e) =>{
+    const [imagePreview, setImagePreview] = useState(props.image);
+    const [status, setStatus] = useState("");
 
+    const checkFile = (image, dataDragDrop = "false") =>{
+        const acceptedImageTypes = ['image/jpeg', 'image/png', 'image/bmp'];
+
+        if(image && acceptedImageTypes.includes(image.type)){
+            setStatus("is-valid");
+            changeImagePreview(image);
+            if(dataDragDrop !== "false"){
+                uploadBtn.current.files = dataDragDrop;
+            }
+        }
+        else{
+            setStatus("is-invalid");
+        }
     }
 
-    console.log(props.image)
+    const changeImagePreview = (image) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+
+        reader.onloadend = () => {
+            setImagePreview([reader.result]);
+        }
+    }
 
     return (
         <Row className="mb-3">
             <Form.Label className="text-md-end text-nowrap" column lg={3}>{props.label} </Form.Label>
             <Col>
-                <Form.File className="d-none" id="formcheck-api-custom" custom>
-                <Form.File.Input ref={uploadBtn} disabled={props.disabled} required name="image" className={props.status + " form-control"} accept="image/png,image/jpeg" onChange={checkFile}/>
+                <Form.Control 
+                    ref={uploadBtn} 
+                    
+                    type="file" 
+                    name="image"
+                    onChange={(e) => checkFile(e.target.files[0])}
+                    disabled={props.disabled}
+                    className="d-none"
+                    accept="image/jpeg, image/png, image/bmp"
+                />
+                {/* <Form.File className="d-none" id="formcheck-api-custom" custom>
+                <Form.File.Input ref={uploadBtn} 
+                    disabled={props.disabled} 
+                    onChange={(e) => checkFile(e.target.files[0])} 
+                    required 
+                    name="image" className={props.status + " form-control"} accept="image/jpeg, image/png, image/bmp"/>
                 <Form.File.Label data-browse="Browse">
                     Choose File
                 </Form.File.Label>
                 <Form.Control.Feedback type="valid">Ready for upload!</Form.Control.Feedback>
                 <Form.Control.Feedback type="invalid">Must be image .jpg or .png!</Form.Control.Feedback>
-                </Form.File>
+                </Form.File> */}
                 <div className="border-image position-relative">
-                    <Image className="hoverImage" src={`${props.image}?${props.hash}`} onError={(e)=>{e.target.onerror = null; e.target.src=imageNotFound}} fluid />
-                    <div id="dragFile" onDragOver={(e)=>{e.preventDefault();}} 
+                    <Image className="hoverImage" src={imagePreview} onError={(e)=>{e.target.onerror = null; e.target.src=imageNotFound}} fluid />
+                    <div ref={uploadDrag} 
+                        onDragOver={(e)=>{
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }} 
                         onDragEnter={(e)=>{
                             e.preventDefault();
-                            // console.log(e.target.id);
-                            // console.log(e.target);
-                            if(e.target.id === "dragFile"){
-                                e.target.style.opacity = "1";
-                            }
-                            // console.log("enter add class");
+                            e.stopPropagation()
+                            uploadDrag.current.style.opacity = "1";
                         }}
                         onDragLeave={(e)=>{
                             e.preventDefault();
-                            // console.log("current ", e.currentTarget)
-                            // console.log("target ", e.target)
-                            // console.log("current Target child ", e.currentTarget.firstChild)
-                            // if(e.target !== e.currentTarget.childNodes){
-
-                            //     e.target.style.opacity = "";
-                            // }
-                            console.log(e.target);
-                            if(e.target.id === "dragFile"){
-                                e.target.style.opacity = "";
+                            e.stopPropagation()
+                            if(e.relatedTarget !== uploadDrag.current.children[0] && e.relatedTarget !== uploadDrag.current){
+                                uploadDrag.current.style.opacity = "";
                             }
-                            // e.target.style.opacity = "";
-                            // console.log("remove class, coba ganti if target == child")
-                            // pake ID coba nanti
                         }}
-                        // onDrop={(e) => {
-                        //     e.preventDefault();
-                        //     e.dataTransfer.dropEffect = "Drop your image here!"
-                        //     console.log(e);
-                        //     // e.target.click();
-                        //     console.log("something drop");
-                        // }} 
+                        onDrop={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation()
+                            // console.log(e.dataTransfer.files[0]);
+                            uploadDrag.current.style.opacity = "";
+                            checkFile(e.dataTransfer.files[0], e.dataTransfer.files);
+                        }} 
                         className="position-absolute imageUploadOverlay d-flex justify-content-center align-items-center align-content-center">
-                        {/* <Button onClick={() => uploadBtn.current.click()} className="btnWhiteTrans px-4 py-2" >Change Image</Button> */}
-                    </div>
-                    <div className="btnUploadImage d-flex align-content-center align-items-center justify-content-center">
                         <Button onClick={() => uploadBtn.current.click()} className="btnWhiteTrans px-4 py-2" >Change Image</Button>
                     </div>
                 </div>
+                <Form.Control className={status+" d-none"}/>
+                <Form.Control.Feedback type="valid">Ready for upload!</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">Must be image .jpg or .png!</Form.Control.Feedback>
                 {/* JADI */}
                 {/* <div className="border-image">
                     <svg className="border-svg">
